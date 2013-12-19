bbb7945a93d4c3153a578d40b0e6ae47cc19049c
Vyacheslav Bobrov <bobrov.vy@gmail.com>
2013-12-19 17:25:14 +0400

    Класс для управления логгерами (хранение, выдача, установка уровня логов)

---
+++ src/com/github/bobrov/vyacheslav/fiction_biblioteca/Loggers.java
@@ -0,0 +1,102 @@
+/**
+ * 
+ */
+package com.github.bobrov.vyacheslav.fiction_biblioteca;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
+
+/**
+ * Логгеры классов, пул для управления и выдачи классам
+ * @author Vyacheslav Bobrov
+ */
+public class Loggers {
+	static final Loggers INSTANCE=new Loggers();
+	static Loggers getInstance(){
+		return INSTANCE;
+	}
+	Loggers() {	
+	}
+	
+	/**Таблица используемых логгеров*/
+	@SuppressWarnings("rawtypes")
+	private Map<Class, Logger> loggers=new HashMap<Class, Logger>();
+	
+	String logConfig;
+	
+	Level logLevel=null;
+	
+	public void setLogConfig(String newLogConfig){
+		if(!newLogConfig.equals(logConfig)){
+			URL url=Loggers.class.getResource(newLogConfig);
+			if(url==null){
+				String err=
+					"Ошибка конфигурации! " +
+					"Невозможно загрузить настройки подсистемы логгирования:"+newLogConfig;
+				
+				getLogger(Loggers.class).error(err);
+				System.err.println(err);
+				
+				return;
+			}
+			DOMConfigurator.configure(url);
+			logConfig=newLogConfig;
+		}
+	}
+
+	/**
+	 * Получить логгер {@link Logger}. <br/> 
+	 * Все объекты системы должны получать логгер у Loggers
+	 * @param cl класс, который запрашивает логгер
+	 * @return {@link Logger}
+	 */
+	public Logger getLogger(@SuppressWarnings("rawtypes") Class cl){		
+		Logger logger=loggers.get(cl);		
+		if(logger==null){
+			logger=Logger.getLogger(cl);
+			loggers.put(cl, logger);
+		}
+		return logger;
+	}
+	
+	/**
+	 * Обновить уровень логгирования у всех используемых системой логгеров
+	 */
+	private void updateLogLevels(){	
+		for(Logger logger:loggers.values())
+			logger.setLevel(logLevel);	
+	}
+	
+	/**
+	 * Установить уровень логгирования для всей системы
+	 * @param logLevel {@link Level}
+	 */
+	public void setLogLevel(Level logLevel){	
+		if(this.logLevel!=null && this.logLevel.equals(logLevel))
+			return;
+		
+		this.logLevel=logLevel;
+		
+		Logger logger=getLogger(Loggers.class);
+		
+		logger.setLevel(Level.INFO);
+		logger.info("Уровень логгирования изменен на:"+logLevel);
+		
+		logger.setLevel(logLevel);
+		updateLogLevels();
+	}
+	
+	/**
+	 * Установить уровень логгирования для всей системы
+	 * @param logLevel {@link String}
+	 */
+	public void setLogLevel(String logLevel){
+		Level newLevel=Level.toLevel(logLevel);
+		setLogLevel(newLevel);
+	}
+}
