362ff08e31f9027070c629bf234339419b3f0842
Vyacheslav Bobrov <bobrov.vy@gmail.com>
2013-12-19 17:26:36 +0400

    Класс для работы с книгами

---
+++ src/com/github/bobrov/vyacheslav/fiction_biblioteca/book_classes/Book.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.github.bobrov.vyacheslav.fiction_biblioteca.book_classes;
+
+/**
+ * @author Vyacheslav Bobrov
+ */
+public class Book {
+
+	/**
+	 * 
+	 */
+	public Book() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
